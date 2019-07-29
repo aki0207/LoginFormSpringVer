961246d16f2c3101f3908025c4286a1688ffc122
aki0207 <forever.look.aki@docomo.ne.jp>
2019-07-29 17:50:09 +0900

    事務所isGoodbye

--- src/main/java/com/example/login/app/login/LoginController.java
+++ src/main/java/com/example/login/app/login/LoginController.java
@@ -2,11 +2,24 @@ package com.example.login.app.login;
 
 import java.util.Collection;
 
+import javax.inject.Inject;
+import javax.validation.Valid;
+
+import com.github.dozermapper.core.Mapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.terasoluna.gfw.common.exception.BusinessException;
+import org.terasoluna.gfw.common.message.ResultMessage;
+import org.terasoluna.gfw.common.message.ResultMessages;
+
+import com.example.login.domain.model.User;
+import com.example.login.domain.service.login.UserService;
 
 
 
@@ -15,6 +28,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("login")
 public class LoginController {
 	
+	@Inject
+	Mapper beanMapper; 
+	
+	@Inject
+	UserService userService;
+	
 	@ModelAttribute 
 	public LoginForm setUpForm() {
 		LoginForm form = new LoginForm();
@@ -22,9 +41,31 @@ public class LoginController {
 	}
 	
 	@GetMapping("create")
-	public String list(Model model) {
+	public String create(Model model) {
 		return "login/create"; 
 	}
+	
+    @PostMapping("create")
+    public String create(@Valid LoginForm loginForm, BindingResult bindingResult,
+            Model model, RedirectAttributes attributes) {
+
+        if (bindingResult.hasErrors()) {
+            return create(model);
+        }
+
+        User user = beanMapper.map(loginForm, User.class);
+
+        try {
+            userService.create(user);
+        } catch (BusinessException e) {
+            model.addAttribute(e.getResultMessages());
+            return create(model);
+        }
+
+        attributes.addFlashAttribute(ResultMessages.success().add(
+                ResultMessage.fromText("Created successfully!")));
+        return "redirect:/login/create";
+    }
 	
 
 }
--- src/main/webapp/WEB-INF/views/login/create.jsp
+++ src/main/webapp/WEB-INF/views/login/create.jsp
@@ -7,12 +7,22 @@
 </head>
 <body>
 	<h1>登録画面</h1>
+	
 	<form:form action="${pageContext.request.contextPath}/login/create"
 			method="post" modelAttribute="loginForm">
+			<h3>アドレスを入力してください</h3>
 			<form:input path="mailAddress" />
 			<form:errors path="mailAddress" cssClass="text-error" />
-	
+			
+			<h3>パスワードを入力してください</h3>
+			<form:input path="password" />
+			<form:errors path="password" cssClass="text-error" />
+			<form:button>登録!</form:button>
 	</form:form>
+	
+	
+	
+	
 
 </body>
 </html>
